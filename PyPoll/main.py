4b935888b4b7c1ef1acf22accbd21b0cd61d2e44
AmorosoData <amoroson@gmail.com>
2020-07-28 12:14:20 -0700

    adding PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,60 +1,58 @@
 import os
 import csv
+from collections import defaultdict
+
 
 csvpath = os.path.join("Resources","election_data.csv")
-file_to_output= os.path.join("Analysis", "Election_Results.txt")
+file_to_output = os.path.join("Analysis", "Election_Results.txt")
+
 with open(csvpath, newline="") as csvfile:
-    csv_reader =csv.reader(csvfile, delimiter=",")
+    csv_reader = csv.reader(csvfile, delimiter=",")
     csv_headers = next(csv_reader)
     #print (f"CSV Header: {csv_headers}"
+
     # Variables
+    votes = {}
+    total_votes = 0
     candidates = []
-    votes = 0
-    
-    # Loop through csv_reader
-    for row in csv_reader:
+
+    for row in csv_reader: 
         # if value in Candidate column is not in candidate_list,
         # append to candidate_list
-        if row[2] not in candidates:
-            votes+=1
-            candidates.append(row[2])
+        total_votes +=1
+        candidates_name = row[2]
+        if candidates_name not in candidates:
+            candidates.append(candidates_name)
+            votes[candidates_name] = 1
         else:
-            votes+=1
-   
-    # count votes per candidate
-    i = 0 
-    num_candidates = len(candidates)
-    candidate_votes = [0]*num_candidates
+            votes[candidates_name] += 1
+    # print(candidates)
+    # print(votes)
+    # print(total_votes)
+    winner_count = 0
+    with open(file_to_output,"w") as txtfile:
+        output_part_one = (
+        f'Election Results\n'
+        f'---------------------------------\n'
+        f'Total Votes: {total_votes}\n'
+        f'---------------------------------\n')
+        txtfile.write(output_part_one)
+        print(output_part_one)
 
-    for i in range(num_candidates):
-        if row[2] == candidates[i]:
-            candidate_votes[i] += 1
-        # print (len(candidate_list)) = 4
-    most_vote = max(candidate_votes)
-    vote_index = candidate_votes.index(most_vote)
-    winner = candidates[vote_index]
+        for i in votes:
+            # print(i,votes.get(i))
+            value = votes.get(i)
+            percentage = round((value/total_votes)*100,2)
+            output_part_two = (
+                f'{i}: {percentage} ({value})\n')
+            txtfile.write(output_part_two)
+            print(output_part_two)
 
-    #print (winner)
-
-    # percent per candidate
-    j = 0
-    percent_of_votes = []
-    for j in range(num_candidates):
-        percent = round(candidate_votes[j]/votes * 100,2)
-        percent_of_votes.append(percent)
-
-output = (
-    f"Election Resultes\n"
-    f"______________________\n"
-    f"Total Votes: {votes}\n"
-    f"Name: {candidates} {percent_of_votes}, {candidate_votes}\n"
-    f"Winner: {winner}\n"
-    )
-
-# Print the output in code
-print(output)
-# Export the results to text file
-with open(file_to_output, 'w') as textfile:
-
-#     # Initialize csv.writer
-   textfile.write(output)
+            if value > winner_count:
+                winner_count = value
+                winner = i
+        output_part_three = (
+        f'---------------------------------\n'
+        f'Winner is {winner}')
+        txtfile.write(output_part_three)
+        print(output_part_three)
